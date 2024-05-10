52f5fb272d38607133170e78b9a57df08de47322
Conrad G <143351775+cnrdgrgry@users.noreply.github.com>
2024-05-10 12:26:50 +0100

    Complete introduction of routes and Router components.

---
+++ src/components/Summit/Summit.jsx
@@ -0,0 +1,9 @@
+import styles from "./Summit.module.css";
+
+export default function Summit() {
+  return (
+    <div className={styles.wrapper}>
+      <p>Summit Else Will Come Here Eventually</p>
+    </div>
+  );
+}
